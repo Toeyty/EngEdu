b438512ecfd2400145b3372e3fb292a966bc06e7
Toeyty <61030236@kmitl.ac.th>
2019-09-02 14:04:52 +0700

    first year semester 1

---
+++ EngEdu/EngEdu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 255x semester 1,I am an EngEdu Fresman.");
+
+        }
+    }
+}
